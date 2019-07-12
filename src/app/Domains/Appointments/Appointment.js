a90d5d7e504f7a7d2c3c2f70104ea09cd598127e
Leandro Henrique <emtudo@gmail.com>
2019-07-12 08:09:38 -0300

    added past and cancelable

--- src/app/Domains/Appointments/Appointment.js
+++ src/app/Domains/Appointments/Appointment.js
@@ -1,9 +1,22 @@
 const Sequelize = require('sequelize')
 const Model = require('../../Support/Domain/Model')
+const { isBefore, subHours } = require('date-fns')
 
 const defines = {
   date: Sequelize.DATE,
   canceled_at: Sequelize.DATE,
+  past: {
+    type: Sequelize.VIRTUAL,
+    get() {
+      return isBefore(this.date, new Date())
+    },
+  },
+  cancelable: {
+    type: Sequelize.VIRTUAL,
+    get() {
+      return isBefore(new Date(), subHours(this.date, 2))
+    },
+  },
 }
 
 class Appointment extends Model {
